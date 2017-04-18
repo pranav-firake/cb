a23d02c9f38cde877d86d500227fbaa7d20cd7a5
Pranav Firake <pranav.firake7@gmail.com>
2017-04-17 21:16:58 -0400

    Update admin.js

--- server-side/site/routes/admin.js
+++ server-side/site/routes/admin.js
@@ -5,6 +5,9 @@ var emailjs = require('emailjs');
 var fileService = require('./upload.js');
 
 
+var redis = require('redis')
+var client = redis.createClient(6379, '127.0.0.1', {})
+
 var Server = mongo.Server,
     Db = mongo.Db,
     ObjectID = mongo.ObjectID;
