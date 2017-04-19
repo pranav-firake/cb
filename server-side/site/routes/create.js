c34f4809863ad8be0bf94c82fea36b0057f36ccd
Pranav Firake <pranav.firake7@gmail.com>
2017-04-19 15:32:30 -0400

    Update create.js

--- server-side/site/routes/create.js
+++ server-side/site/routes/create.js
@@ -28,15 +28,7 @@ var emailServer  = emailjs.server.connect({
 });
 
 exports.createStudy = function(req, res) {
-try{
-        client.get('canCreate', function(err, reply) {
-        // reply is null when the key is missing
-        if(reply == null || reply == 'false')
-        {
-          res.send('[REDIS] Operation Not Permitted')        
-        }
-        else
-        {
+
 
     var invitecode = req.body.invitecode; 
     var studyKind = req.body.studyKind;
@@ -79,15 +71,7 @@ try{
 
         });
     });
-  }
-
-        console.dir("Value of createStudy: " + reply);
-      });
-    }
-    catch(e)
-    {
-      res.send('Error Encountered!')
-    }
+ 
 };
 
 
